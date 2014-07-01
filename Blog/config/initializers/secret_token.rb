d3b016c4af6dc42bb39fdccf48643ac083d634cb
Charlie Brodie <charliembrodie@gmail.com>
2014-07-01 11:09:20 +0100

    I've added a rudimentary rails blog to the site and I'm about to start extending it and merging the two together

---
+++ Blog/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = 'f1edf4e2caca5d812178e02f85c744afc927e147791bcee57dbf200773376e385b0fb535db87b9a786c0e251312708815f52088e99bfb89dcb2b3bc41b40aa75'
